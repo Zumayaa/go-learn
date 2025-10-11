bd3113c34da11ac6529dc50dcb647c7a7032556a
Zumayaa <abzu_21@alu.uabcs.mx>
2025-10-10 21:48:42 -0700

    simom

--- directory/main.go
+++ directory/main.go
@@ -3,33 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Wep")
 
 	var (
-		//name   string
-		//number string
 		choose int
 	)
 
 	directory := make(map[string]string)
 
 	for choose != 5 {
+		fmt.Println("")
 		fmt.Println("Welcome to Zumayas Directory")
 		fmt.Println("1- Consult the directory")
 		fmt.Println("2- Add a new number")
 		fmt.Println("3- Delete a number")
 		fmt.Println("4- Modify a number")
 		fmt.Println("5- Exit")
-		fmt.Print("Make a choose: ")
+		fmt.Print("Make a choice: ")
 		fmt.Scan(&choose)
+		fmt.Println("")
 
 		switch choose {
 		case 1:
 			consultNumber(directory)
 		case 2:
-			addNumber()
+			addNumber(directory)
 		case 3:
-			deleteNumber()
+			deleteNumber(directory)
 		case 4:
 			modifyNumber()
 		}
@@ -38,17 +37,41 @@ func main() {
 }
 
 func consultNumber(directory map[string]string) {
-	for k, v := range directory {
-		fmt.Println("Nombre:", k, "| Número:", v)
+	if len(directory) != 0 {
+		for k, v := range directory {
+			fmt.Println("Name:", k, "| Number:", v)
+		}
+	} else {
+		fmt.Println("Empty xd")
 	}
 }
 
-func addNumber() {
-	fmt.Println("Here u can add a new number")
+func addNumber(directory map[string]string) {
+	var (
+		name     string
+		apellido string
+	)
+
+	fmt.Println("Write a name: ")
+	fmt.Scan(&name)
+	fmt.Println("Write a number: ")
+	fmt.Scan(&apellido)
+	directory[name] = apellido
+
 }
 
-func deleteNumber() {
-	fmt.Println("Here u can delete a number")
+func deleteNumber(directory map[string]string) {
+	var (
+		name string
+	)
+
+	if len(directory) != 0 {
+		fmt.Println("Write a name to delete: ")
+		fmt.Scan(&name)
+		delete(directory, name)
+	} else {
+		fmt.Println("Empty xd")
+	}
 }
 
 func modifyNumber() {
