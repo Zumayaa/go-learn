9d6b02502c7a82f4e521350819286be3c17d1128
Zumayaa <abzu_21@alu.uabcs.mx>
2025-10-21 21:59:54 -0700

    soy un genio de la programacion que buena esta la de champagne poetry de drake

---
+++ to-do/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	ID          int
+	Description string
+	Done        bool
+}
+
+var tasks []Task
+var idList = 1
+
+func main() {
+	var choice int
+
+	for {
+		menu(&choice)
+
+		switch choice {
+		case 1:
+			add()
+		case 2:
+
+		}
+	}
+
+}
+
+func menu(choice *int) {
+	if len(tasks) > 0 {
+		fmt.Println("\nTareas ingresadas:")
+		for _, task := range tasks {
+			fmt.Printf("%d: %s (Completada: %t)\n", task.ID, task.Description, task.Done)
+		}
+	} else {
+		fmt.Println("No hay tareas agregadas a la lista")
+	}
+
+	fmt.Println("--Biendosveni al To-Do list--")
+	fmt.Println("1- Agregar tarea")
+	fmt.Println("2- Eliminar tarea")
+	fmt.Println("3- Editar tarea")
+	fmt.Println("Escoge una opción: ")
+	fmt.Scan(choice)
+}
+
+func delete() {
+	fmt.Println("Wep")
+}
+
+func add() {
+	var description string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Para salir de esta interfaz escribe 'quit': ")
+
+	for {
+		fmt.Println("Agrega una tarea negro")
+		scanner.Scan()
+		description = scanner.Text()
+
+		if description == "quit" {
+			break
+		}
+
+		if description != "" {
+			task := Task{
+				ID:          idList,
+				Description: description,
+				Done:        false,
+			}
+			tasks = append(tasks, task)
+			idList++
+		}
+	}
+}
